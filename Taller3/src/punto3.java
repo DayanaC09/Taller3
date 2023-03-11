feb0834951bd18469968f2de98d0f7e9fbbd9443
DayanaC09 <cortesdayana80@gmail.com>
2023-03-10 19:16:41 -0500

    taller3

---
+++ Taller3/src/punto3.java
@@ -0,0 +1,12 @@
+
+public class punto3 {
+	
+	public static void main (String[] args)
+	{
+		String cadena1 = "abc";
+		String cadena2 = "def";
+		
+		System.out.println("Hola: "+ cadena1 + cadena2 + "g");
+	}
+
+}
